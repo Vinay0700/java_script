6f67405495b939a84cc9bbf8ff3cdf37d1bf935b
Vinay0700 <89821304+Vinay0700@users.noreply.github.com>
2023-09-05 13:15:00 +0000

    checked type of

--- learn.js/learn1.js
+++ learn.js/learn1.js
@@ -1,4 +1,7 @@
-console.log(1+"2");
-console.log(1+"2"+1);
-console.log("1"+2+2)
-console.log(1+2+"2")
+// console.log(1+"2");
+// console.log(1+"2"+1);
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+
